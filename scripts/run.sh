3aad55d14f26060f1cd5fbbde0152b4e66e82e21
rachael <komimi.p@gmail.com>
2019-05-14 11:56:14 +0800

    change run script from updater to upgrader

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
-## How to run bitmarkNodeWatcher as a docker container
+## How to run bitmarkNodeUpgrader as a docker container
 ## Setup your nase mount directory (here is staging directory)
 # -v $nodeDir/watcherlog:/var/log \
 nodeDir=$HOME/bitmark-node-data
-docker run -d --name bitmarkNodeUpdater \
+docker run -d --name bitmarkNodeUpgrader \
     -e DOCKER_HOST="unix:///var/run/docker.sock" \
-    -e NODE_IMAGE="bitmark/bitmark-node-test" \
-    -e NODE_NAME="bitmarkNodeTest" \
+    -e NODE_IMAGE="bitmark/bitmark-node" \
+    -e NODE_NAME="bitmarkNode" \
     -e USER_NODE_BASE_DIR=$nodeDir \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $nodeDir/data:/.config/bitmark-node/bitmarkd/bitmark/data \
     -v $nodeDir/data-test:/.config/bitmark-node/bitmarkd/testing/data \
-    bitmark/bitmark-node-updater
+    bitmark/bitmark-node-upgrader
